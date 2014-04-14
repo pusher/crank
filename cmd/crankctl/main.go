32cbfd4e58d025f0e85c871270b8b779771c1557
zimbatm <zimbatm@zimbatm.com>
2014-04-14 15:32:28 +0100

    First tentative at command dispatching

--- cmd/crankctl/main.go
+++ cmd/crankctl/main.go
@@ -1,31 +1,82 @@
 package main
 
 import (
+	//	"../../pkg/crank"
 	"flag"
+	"fmt"
 	"log"
 	"net/rpc"
+	"os"
 )
 
+type Command func(*rpc.Client) error
+type CommandSetup func(*flag.FlagSet) Command
+
 var run string
+var commands map[string]CommandSetup
 
 func init() {
-	flag.StringVar(&run, "run", "", "path to control socket")
+	defaultFlags(flag.CommandLine)
+
+	commands = make(map[string]CommandSetup)
+	commands["echo"] = Echo
+}
+
+func defaultFlags(flagSet *flag.FlagSet) {
+	flagSet.StringVar(&run, "run", run, "path to control socket")
+}
+
+func fail(reason string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, reason, args...)
+	flag.Usage()
+	os.Exit(1)
 }
 
 func main() {
+	var err error
 	flag.Parse()
+
+	command := flag.Arg(0)
+
+	if command == "" {
+		fail("command missing\n")
+	}
+
+	cmdSetup, ok := commands[command]
+	if !ok {
+		fail("unknown command %s\n", command)
+	}
+
+	flagSet := flag.NewFlagSet(os.Args[0]+" "+command, flag.ExitOnError)
+	defaultFlags(flagSet)
+	cmd := cmdSetup(flagSet)
+
+	if err = flagSet.Parse(flag.Args()); err != nil {
+		fail("oops: %s\n", err)
+	}
 
 	client, err := rpc.Dial("unix", run)
 	if err != nil {
-		log.Fatal("Couldn't connect: ", err)
+		fail("Couldn't connect: %s\n", err)
 	}
 
-	msg := flag.Arg(0)
-	var reply string
-
-	err = client.Call("crank.Echo", msg, &reply)
-	if err != nil {
-		log.Fatal("echo error:", err)
+	if err = cmd(client); err != nil {
+		fail("command failed: %v\n", err)
 	}
-	log.Println("echo reply: ", reply)
+}
+
+func Echo(flag *flag.FlagSet) Command {
+	var msg string
+	flag.StringVar(&msg, "msg", "foo", "message to send")
+
+	return func(client *rpc.Client) (err error) {
+		var reply string
+
+		if err = client.Call("crank.Echo", &msg, &reply); err != nil {
+			return
+		}
+
+		log.Println("echo reply: ", reply)
+		return
+	}
 }
