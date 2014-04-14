29abac23333bbf91085e401b12eaf32e2d63a937
zimbatm <zimbatm@zimbatm.com>
2014-04-14 12:24:33 +0100

    Removes unused BindRPCSocket function

--- pkg/crank/rpc.go
+++ pkg/crank/rpc.go
@@ -2,7 +2,6 @@ package crank
 
 import (
 	"fmt"
-	"net"
 )
 
 type RPC struct {
@@ -17,13 +16,4 @@ func (self *RPC) Echo(msg *string, reply *string) error {
 	fmt.Println("Got ", *msg)
 	*reply = "Thanks !"
 	return nil
-}
-
-func BindRPCSocket(path string) (l net.Listener, err error) {
-	addr, err := net.ResolveUnixAddr("unix", path)
-	if err != nil {
-		return
-	}
-	l, err = net.ListenUnix("unix", addr)
-	return
 }
