22f104683b9fbafa56fbd6816b7f44debad4580e
zimbatm <zimbatm@zimbatm.com>
2014-05-29 13:46:32 +0100

    Fixing tests and adds a phony `make test` target

--- pkg/crank/process_config_test.go
+++ pkg/crank/process_config_test.go
@@ -8,10 +8,14 @@ func TestProcessConfigCloning(t *testing.T) {
 	c := &ProcessConfig{"hello", []string{"world"}, 1, 2}
 
 	c2 := c.clone()
-	c2.Command = "bob"
+	c2.Command = []string{"bob"}
 
 	if c == c2 {
-		t.Fail()
+		t.Error(c, c2)
+	}
+
+	if c2.StartTimeout != 1 {
+		t.Error("start timeout", c2.StartTimeout)
 	}
 
 }
